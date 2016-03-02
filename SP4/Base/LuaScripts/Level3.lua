7bdae40be7cc4f3054ad83d072cd4a8524eccf8e
Tan Kin Lek <tankinlek1997@gmail.com>
2016-03-02 09:49:34 +0800

    Level 3 and 4 added

--- SP4/Base/LuaScripts/Level3.lua
+++ SP4/Base/LuaScripts/Level3.lua
@@ -1,98 +1,41 @@
-tileMapWidth = 14
-tileMapHeight = 18
+tileMapWidth = 10
+tileMapHeight = 12
 
-playerPosX = 5
-playerPosY = 5
+playerPosX = 1
+playerPosY = 10
 
 MinMoves = 30
 
 maxNumOfZombies = 1
-maxNumOfRetardZombies = 1
+maxNumOfRetardZombies = 0
 maxNumOfBlocks = 2
-maxNumOfCoins = 4
-maxNumOfFire = 1
-maxNumOfDemonFire = 1
+maxNumOfCoins = 2
+maxNumOfFire = 0
+maxNumOfDemonFire = 0
 maxNumOfRedKeys = 1
-maxNumOfBlueKeys = 1
-maxNumOfGreenKeys = 1
-maxNumOfYellowKeys = 9
+maxNumOfBlueKeys = 0
+maxNumOfGreenKeys = 0
+maxNumOfYellowKeys = 0
 
-zombie1PosX = 2
-zombie1PosY = 2
+zombie1PosX = 5
+zombie1PosY = 5
 zombie1Hold = 1
 
 retardZombie1PosX = 2
 retardZombie1PosY = 3
 retardZombie1Hold = 2
 
-fire1PosX = 9
-fire1PosY = 9
-
-demonFire1PosX = 10
-demonFire1PosY = 9
-
 redKey1PosX = 8
 redKey1PosY = 8
 
-blueKey1PosX = 9
-blueKey1PosY = 8
-
-greenKey1PosX = 10
-greenKey1PosY = 8
-
-yellowKey1PosX = 11
-yellowKey1PosY = 8
-
-block1PosX = 2
-block1PosY = 8
+block1PosX = 3
+block1PosY = 7
 
 block2PosX = 8
-block2PosY = 2
+block2PosY = 9	
 
 coin1PosX = 6
-coin1PosY = 8
+coin1PosY = 9
 
-coin2PosX = 7
+coin2PosX = 8
 coin2PosY = 7
-
-coin3PosX = 8
-coin3PosY = 7
-
-coin4PosX = 9
-coin4PosY = 7
-
-blueKey1PosX = 9
-blueKey1PosY = 8
-
-greenKey1PosX = 10
-greenKey1PosY = 8
-
-yellowKey1PosX = 11
-yellowKey1PosY = 1
-
-yellowKey2PosX = 11
-yellowKey2PosY = 7
-
-yellowKey3PosX = 11
-yellowKey3PosY = 6
-
-yellowKey4PosX = 11
-yellowKey4PosY = 5
-
-yellowKey5PosX = 11
-yellowKey5PosY = 4
-
-yellowKey6PosX = 11
-yellowKey6PosY = 3
-
-yellowKey7PosX = 11
-yellowKey7PosY = 2
-
-yellowKey8PosX = 11
-yellowKey8PosY = 8
-
-yellowKey9PosX = 11
-yellowKey9PosY = 9
-
-yellowKey10PosX = 11
-yellowKey10PosY = 10
---
+++ SP4/Base/LuaScripts/Level4.lua
@@ -0,0 +1,36 @@
+tileMapWidth = 13
+tileMapHeight = 13
+
+playerPosX = 6
+playerPosY = 7
+
+MinMoves = 30
+
+maxNumOfZombies = 0
+maxNumOfRetardZombies = 4
+maxNumOfBlocks = 1
+maxNumOfFire = 0
+maxNumOfDemonFire = 0
+maxNumOfRedKeys = 0
+maxNumOfBlueKeys = 0
+maxNumOfGreenKeys = 0
+maxNumOfYellowKeys = 0
+
+retardZombie1PosX = 4
+retardZombie1PosY = 2
+retardZombie1Hold = 2
+
+retardZombie2PosX = 8
+retardZombie2PosY = 2
+retardZombie2Hold = 2
+
+retardZombie3PosX = 1
+retardZombie3PosY = 8
+retardZombie3Hold = 2
+
+retardZombie4PosX = 11
+retardZombie4PosY = 8
+retardZombie4Hold = 2
+
+block1PosX = 6
+block1PosY = 8
