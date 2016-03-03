db08eb8c4017b8ede5a2525526915ffe3eea63a3
vinceajy93 <vinceajy93@hotmail.com>
2016-03-03 16:03:29 +0800

    -Added lv 7 to the game

---
+++ SP4/Base/LuaScripts/Level7.lua
@@ -0,0 +1,88 @@
+tileMapWidth = 15
+tileMapHeight = 22
+
+playerPosX = 2
+playerPosY = 3
+
+MinMoves = 50
+
+maxNumOfZombies = 0
+maxNumOfRetardZombies = 0
+maxNumOfBlocks = 3
+maxNumOfCoins = 0
+maxNumOfFire = 9
+maxNumOfDemonFire = 5
+maxNumOfRedKeys = 1
+maxNumOfBlueKeys = 1
+maxNumOfGreenKeys = 0
+maxNumOfYellowKeys = 0
+maxNumOfTextBox = 0
+
+block1PosX = 3
+block1PosY = 2
+block2PosX = 3
+block2PosY = 4
+block3PosX = 3
+block3PosY = 3
+
+redKey1PosX = 13
+redKey1PosY = 7
+
+blueKey1PosX = 3
+blueKey1PosY = 14
+
+fire1PosX = 11
+fire1PosY = 10
+fire1Stage = 1
+
+fire2PosX = 9
+fire2PosY = 11
+fire2Stage = 1
+
+fire3PosX = 7
+fire3PosY = 14
+fire3Stage = 1
+
+fire4PosX = 5
+fire4PosY = 10
+fire4Stage = 1
+
+fire5PosX = 5
+fire5PosY = 11
+fire5Stage = 1
+
+fire6PosX = 3
+fire6PosY = 10
+fire6Stage = 1
+
+fire7PosX = 3
+fire7PosY = 11
+fire7Stage = 1
+
+fire8PosX = 5
+firePosY = 12
+fire8Stage = 1
+
+fire9PosX = 3
+fire9PosY = 12
+fire9Stage = 1
+
+demonFire1PosX = 2
+demonFire1PosY = 20
+demonFire1Stage = 1
+
+demonFire2PosX = 3
+demonFire2PosY = 20
+demonFire2Stage = 2
+
+demonFire3PosX = 5
+demonFire3PosY = 20
+demonFire3Stage = 2
+
+demonFire4PosX = 6
+demonFire4PosY = 20
+demonFire4Stage = 1
+
+demonFire5PosX = 10
+demonFire5PosY = 20
+demonFire5Stage = 2
